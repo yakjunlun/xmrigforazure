b429b503f7c3a788e834412b3c658a8fb6439064
Yak Jun Lun <junlun_yak@mymail.sutd.edu.sg>
2020-06-23 00:20:55 +0800

    optimise

--- buildxmrig.sh
+++ buildxmrig.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 sudo apt-get -y update
 sudo apt-get -y upgrade    
-sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+sudo apt-get install -y git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
 git clone https://github.com/xmrig/xmrig.git
 
-cd xmrig/src
+cd xmrig && cd src && ls
 rm ./donate.h
 wget https://raw.githubusercontent.com/yakjunlun/xmrigforazure/master/donate.h
 cd ..
-
+sudo ./scripts/enable_1gb_pages.sh
+sudo ./scripts/randomx_boost.sh
 mkdir build && cd build
 cmake ..
 make -j$(nproc)
-sudo ./scripts/enable_1gb_pages.sh
-sudo ./scripts/randomx_boost.sh
 
 wget https://raw.githubusercontent.com/yakjunlun/xmrigforazure/master/config.json
 
